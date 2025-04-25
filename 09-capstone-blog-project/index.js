82806d77607e4379ffa6ce0bf65f819c53fa4c9b
Giordana Rubin <giordana1g@gmail.com>
2025-04-25 18:59:45 -0300

    atualizões no capstone project

--- 09-capstone-blog-project/index.js
+++ 09-capstone-blog-project/index.js
@@ -1,10 +1,19 @@
 import express from "express";
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
-app.get("/", (req, res) => {
+//lembrar que cada ação é uma rota!
+app.get("/home", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/new-post", (req, res) => {
+  res.render("new-post.ejs");
 });
 
 app.listen(port, () => {
